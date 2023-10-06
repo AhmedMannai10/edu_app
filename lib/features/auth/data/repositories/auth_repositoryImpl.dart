d12c7a23bf5b79d6125bc0d5e1cfd09d6ebccd4e
DrGhost <mannaiahmed7518@gmail.com>
2023-10-06 18:38:17 +0100

    implementing data authentication repository

--- lib/features/auth/data/repositories/auth_repositoryImpl.dart
+++ lib/features/auth/data/repositories/auth_repositoryImpl.dart
@@ -1,14 +1,28 @@
 import 'package:dartz/dartz.dart';
 import 'package:edu_app/core/error/failure.dart';
-import 'package:edu_app/features/auth/domain/entities/login.dart';
+import 'package:edu_app/features/auth/data/models/login_model.dart';
 import 'package:edu_app/features/auth/domain/entities/sign_up.dart';
 import 'package:edu_app/features/auth/domain/repositories/auth_repository.dart';
 
+import '../datasources/auth_remote_data_source.dart';
+import '../models/auth_response_model.dart';
+
 class AuthRepositoryImpl extends AuthRepository {
+  final AuthRemoteDataSource authRemoteDataSource;
+
+  AuthRepositoryImpl({required this.authRemoteDataSource});
+
   @override
-  Future<Either<Failure, Login>> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<Either<Failure, AuthResponseModel>> login(
+      String email, String password) async {
+    try {
+      LoginModel loginModel = LoginModel(email: email, password: password);
+      final AuthResponseModel authResponseModel =
+          await authRemoteDataSource.login(loginModel);
+      return Right(authResponseModel);
+    } on ServerException {
+      return const Left(ServerFailure("Unable to get data from remote"));
+    }
   }
 
   @override
